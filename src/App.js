1daf1225a3307378169b957230854da1ff2cba6c
Sithu Lwin <lwinsithu3@gmail.com>
2019-11-27 20:38:54 +0630

    Item added

--- src/App.js
+++ src/App.js
@@ -2,31 +2,72 @@ import React from 'react';
 import Stuff from './Stuff';
 import About from './About';
 import Home from './Home';
+import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App() {
+  const styles = {
+    paperContainer: {
+        backgroundImage: 'url("./image/ml.JPG")'
+    }
+};
+
   return (
   
     <Router>     
- <div class="container">
-            <div class="row">
-              <div class="col-4">
-          <ul >
-            <li ><Link to={'/'} > Home </Link></li>
-            <li ><Link to={'/contact'} >Contact</Link></li>
-            <li><Link to={'/about'} >About</Link></li>
-          </ul>
+
+            <div class="row container-fluid">
+              <div class="col-2">
+              <h3 className="head" style={styles.paperContainer}>စာပေမြင့်မှ လူမျိုးတင့်မည်</h3>
+              <div className="title-bound" >
+                
+                <hr></hr>
+           <h3><Link to={'/'} >ဝတ္ထုတို</Link></h3>
+           <hr></hr>
+           <h3> <Link to={'/contact'} >ဝတ္ထုရှည်</Link></h3>
+           <hr></hr>
+           <h3><Link to={'/about'} > သုတ</Link></h3> 
+           <hr></hr>
+           <h3><Link to={'/about'} > ရသ/အက်ဆေး</Link></h3> 
+           <hr></hr>
+           <h3><Link to={'/about'} > ဘာသာပြန်</Link></h3> 
+           <hr></hr>
+           <h3><Link to={'/about'} > ဆုရစာအုပ်များ</Link></h3> 
+           <hr></hr>
+           <h3><Link to={'/about'} > အချစ်ဝတ္ထု</Link></h3> 
+           <hr></hr>
+           <h3><Link to={'/about'} > ဟာသစာအုပ်</Link></h3> 
+           <hr></hr>
+           <h3><Link to={'/about'} >အတ္ထုပတ္တိစာအုပ်</Link></h3> 
+           <hr></hr>
+           <h3><Link to={'/about'} > တရားစာအုပ်</Link></h3> 
+           <hr></hr>
+           <h3><Link to={'/about'} > သိုင်းစာအုပ်</Link></h3> 
+           <hr></hr>
+           <h3><Link to={'/about'} > page အကြောင်း</Link></h3> 
+         {/* သုတ
+ရသ/အက်ဆေး
+ဘာသာပြန်
+ဆုရစာအုပ်များ
+အချစ်ဝတ္ထု
+ဟာသစာအုပ်
+သိုင်းစာအုပ်
+အတ္ထုပတ္တိစာအုပ်
+တရားစာအုပ် */}
+          </div>
           </div>
           <Switch>
-            <div>
+            <div class="col-9">
+              <div className="showpage">
               <Route exact path='/' component={Home} />
               <Route path='/contact' component={Stuff} />
               <Route path='/about' component={About} />
               </div>
+              </div>
           </Switch>
           </div>
-          </div>
+          
       </Router>
     
   );
