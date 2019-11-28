0e3a7deb0464963f6a95b6f962076c64a00e024b
Sithu Lwin <lwinsithu3@gmail.com>
2019-11-29 03:37:11 +0630

    Finish

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React from 'react';
 import Shortnovel from './Js/Shortnovel';
 import Novel from './Js/Novel';
 import Knowledge from './Js/Knowledge';
+import Essay from './Js/Essay';
+import Cover from './Js/Cover';
+import Award from './Js/Award';
+import Love from './Js/Love';
+import Funny from './Js/Funny';
+import Biography from './Js/Biography';
+import Damah from './Js/Damah';
+import Thei from './Js/Thei';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -29,23 +37,21 @@ export default function App() {
            <hr></hr>
            <h3><Link to={'/knowledge'} > သုတ</Link></h3> 
            <hr></hr>
-           <h3><Link to={'/about'} > ရသ/အက်ဆေး</Link></h3> 
+           <h3><Link to={'/essay'} > ရသ/အက်ဆေး</Link></h3> 
            <hr></hr>
-           <h3><Link to={'/about'} > ဘာသာပြန်</Link></h3> 
+           <h3><Link to={'/cover'} > ဘာသာပြန်</Link></h3> 
            <hr></hr>
-           <h3><Link to={'/about'} > ဆုရစာအုပ်များ</Link></h3> 
+           <h3><Link to={'/award'} > ဆုရစာအုပ်များ</Link></h3> 
            <hr></hr>
-           <h3><Link to={'/about'} > အချစ်ဝတ္ထု</Link></h3> 
+           <h3><Link to={'/love'} > အချစ်ဝတ္ထု</Link></h3> 
            <hr></hr>
-           <h3><Link to={'/about'} > ဟာသစာအုပ်</Link></h3> 
+           <h3><Link to={'/funny'} > ဟာသစာအုပ်</Link></h3> 
            <hr></hr>
-           <h3><Link to={'/about'} >အတ္ထုပတ္တိစာအုပ်</Link></h3> 
+           <h3><Link to={'/biography'} >အတ္ထုပတ္တိစာအုပ်</Link></h3> 
            <hr></hr>
-           <h3><Link to={'/about'} > တရားစာအုပ်</Link></h3> 
+           <h3><Link to={'/damah'} > တရားစာအုပ်</Link></h3> 
            <hr></hr>
-           <h3><Link to={'/about'} > သိုင်းစာအုပ်</Link></h3> 
-           <hr></hr>
-           <h3><Link to={'/about'} > page အကြောင်း</Link></h3> 
+           <h3><Link to={'/thei'} > သိုင်းစာအုပ်</Link></h3> 
          {/* သုတ
 ရသ/အက်ဆေး
 ဘာသာပြန်
@@ -63,6 +69,14 @@ export default function App() {
               <Route exact path='/' component={Shortnovel} />
               <Route exact path='/novel' component={Novel} />
               <Route exact path='/knowledge' component={Knowledge} />
+              <Route exact path='/essay' component={Essay} />
+              <Route exact path='/cover' component={Cover} />
+              <Route exact path='/award' component={Award} />
+              <Route exact path='/love' component={Love} />
+              <Route exact path='/funny' component={Funny} />
+              <Route exact path='/biography' component={Biography} />
+              <Route exact path='/damah' component={Damah} />
+              <Route exact path='/thei' component={Thei} />
               </div>
               </div>
           </Switch>
---
+++ src/Js/Award.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import * as firebase from 'firebase'
+import '../css/Showpage.css'
+
+export class Award extends Component {
+    constructor(props) {
+        super(props)
+      
+        this.state = {
+           words:[]
+        }
+      }
+      componentDidMount(){
+          const wordRef=firebase.database().ref("React/Award");
+          wordRef.on('value',(snapshot)=>{
+              let words=snapshot.val();
+              let newstate=[];
+              for(let word in words){
+                  newstate.push({
+                    link:words[word].link,
+                      photo:words[word].photo,
+                      proposal:words[word].proposal,
+                      title:words[word].title
+                  });
+              }
+              this.setState({
+                  words:newstate
+              });
+          });
+      }
+      render() {
+          return (
+              <div>
+                 
+                 {this.state.words.map((word)=>{
+                     return(
+                      <div >
+                                       <div className="font"> 
+                                           <p><b>စာအုပ်အမည် - </b>{word.title}</p>
+                                           <p ><b>Review - </b>{word.proposal}</p>
+                                           <p ><b>Link - </b>{word.link}</p>
+                                          {/* <p><img src="{word.photo}"></img></p> */}
+                                         <p ><img  alt="900x500"  src={word.photo}/></p> 
+                                         {/* style={{textAlign:"center"}} */}
+                                         <hr></hr>
+                                           </div> 
+                                      </div>
+                     )
+                 })}
+          
+              </div>
+          )
+      }
+  }
+
+
+export default Award
